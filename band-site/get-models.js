285303f0d035171011fd8945eb304873d93b5e9e
Karim Dridi <kamdridi@hotmail.com>
2026-07-22 09:37:51 -0400

    Fix typescript error in models API

--- band-site/get-models.js
+++ band-site/get-models.js
@@ -1,1 +1,1 @@
-fetch('https://generativelanguage.googleapis.com/v1beta/models?key=' + process.env.GEMINI_API_KEY).then(r=>r.json()).then(j=>console.log('MODEL_LIST_DUMP:', j.models.map(m=>m.name).join(',')))
+fetch('https://generativelanguage.googleapis.com/v1beta/models?key=' + process.env.GEMINI_API_KEY).then(r=>r.json()).then(j=>console.log('MODEL_LIST_DUMP:', JSON.stringify(j)))
