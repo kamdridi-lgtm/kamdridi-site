232e639660a838011d8cf31bd0758a31417dacb8
kamdridi-lgtm <kamdridi@hotmail.com>
2026-08-11 13:30:24 -0400

    seo: strengthen KAM DRIDI artist entity metadata and structured data

--- band-site/app/layout.tsx
+++ band-site/app/layout.tsx
@@ -23,39 +23,90 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    title: "KAMDRIDI App",
+    title: "KAM DRIDI",
     statusBarStyle: "black-translucent"
   },
   title: {
-    default: `${siteMeta.bandName} | Official Website`,
-    template: `%s | ${siteMeta.bandName}`
+    default: "KAM DRIDI | Official Website",
+    template: "%s | KAM DRIDI"
   },
   description: siteMeta.description,
   keywords: siteMeta.keywords,
+  authors: [{ name: "KAM DRIDI", url: metadataBase }],
+  creator: "KAM DRIDI",
+  publisher: "KAM DRIDI",
+  category: "music",
   alternates: {
     canonical: "/"
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1
+    }
+  },
   openGraph: {
-    title: `${siteMeta.bandName} | Official Website`,
+    title: "KAM DRIDI | Official Website",
     description: siteMeta.description,
     url: metadataBase,
-    siteName: siteMeta.bandName,
-    locale: "en_US",
+    siteName: "KAM DRIDI",
+    locale: "en_CA",
     type: "website",
     images: [
       {
         url: "/assets/images/releases/echoes-unearthed-cover.jpg",
         width: 1200,
         height: 630,
-        alt: `${siteMeta.bandName} - ${siteMeta.albumName}`
+        alt: `KAM DRIDI - ${siteMeta.albumName}`
       }
     ]
   },
   twitter: {
     card: "summary_large_image",
-    title: `${siteMeta.bandName} | Official Website`,
+    title: "KAM DRIDI | Official Website",
     description: siteMeta.description,
     images: ["/assets/images/releases/echoes-unearthed-cover.jpg"]
+  }
+};
+
+const artistStructuredData = {
+  "@context": "https://schema.org",
+  "@type": "MusicGroup",
+  "@id": `${siteUrl}/#artist`,
+  name: "KAM DRIDI",
+  alternateName: "KAMDRIDI",
+  url: siteUrl,
+  image: `${siteUrl}/assets/images/kamdridi-pwa-icon.png`,
+  description: siteMeta.description,
+  genre: ["Cinematic Rock", "Hard Rock", "Industrial Rock"],
+  foundingLocation: {
+    "@type": "Place",
+    name: "Montreal, Quebec, Canada"
+  },
+  sameAs: [
+    "https://youtube.com/@kamdridi",
+    "https://instagram.com/kamdridi",
+    "https://tiktok.com/@kamdridi",
+    "https://music.apple.com/us/artist/kam-dridi/1871722663",
+    "https://facebook.com/kamdridi",
+    "https://x.com/kamdridi"
+  ]
+};
+
+const websiteStructuredData = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "@id": `${siteUrl}/#website`,
+  url: siteUrl,
+  name: "KAM DRIDI",
+  alternateName: "KAMDRIDI",
+  publisher: {
+    "@id": `${siteUrl}/#artist`
   }
 };
 
@@ -63,6 +114,14 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(artistStructuredData) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteStructuredData) }}
+        />
         <Providers>
           <DemographicsTracker />
           <UnLetterHighlighter />
