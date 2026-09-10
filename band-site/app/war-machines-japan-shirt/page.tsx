6f81c89bbaa2f170e6f88950ce149974aad1242e
kamdridi-lgtm <kamdridi@hotmail.com>
2026-09-10 00:44:49 -0400

    Add direct War Machines Japan shirt link

---
+++ band-site/app/war-machines-japan-shirt/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function WarMachinesJapanShirtRedirect() {
+  redirect("/store/17-for-ever-design-long-sleeve-tee-retro-maxi-single-graphic");
+}
